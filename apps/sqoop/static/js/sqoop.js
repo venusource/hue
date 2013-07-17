f4c0e27be9d0317a4f44a890801acfd65bb4e2f8
Enrico Berti <hello@enricoberti.com>
2013-07-17 19:59:52 +0200

    HUE-1395 [sqoop] Improve UI
    Initial commit

--- apps/sqoop/static/js/sqoop.js
+++ apps/sqoop/static/js/sqoop.js
@@ -36,6 +36,7 @@ function showMainSection(mainSection) {
 
 function showSection(mainSection, section) {
   showMainSection(mainSection);
+  $(document).trigger('show_section', section);
   if ($("#" + section).is(":hidden")) {
     $(".section").hide();
     $("#" + section).show();
@@ -91,6 +92,7 @@ var viewModel = new (function() {
   self.connection = ko.observable();
   self.editConnection = ko.observable();
   self.filter = ko.observable("");
+  self.shownSection = ko.observable("");
   self.isDirty = ko.observable(false);
   // Must always have a value.
   self.connector = ko.computed(function() {
